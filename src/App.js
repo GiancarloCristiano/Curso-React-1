3643085f0244738c97199c627562317498b44281
Giancarlo <gian-k@.live.com>
2020-12-12 01:16:18 -0300

    1ra levantada

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Esta es mi primera app con React :D
         </p>
         <a
           className="App-link"
